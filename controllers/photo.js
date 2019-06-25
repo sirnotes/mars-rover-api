77441c4dc4f877f4091c66d9fe5ba87154ec8788
Ken Nichol <ken.nichol@live.ca>
2019-06-25 16:29:28 -0700

    remove extra console.log calls

--- controllers/photo.js
+++ controllers/photo.js
@@ -3,12 +3,9 @@ const apiKey = process.env.NASA_API_KEY || 'DEMO_KEY';
 
 const handlePhoto = () => (req, res) => {
     const { sol, camera } = req.body;
-
-    console.log(req.body);
     let url = new URL('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos');
     let params = { api_key: apiKey, sol: sol, camera: camera };
     Object.keys(params).forEach(key => { if (null !== params[key]) { url.searchParams.append(key, params[key]) } })
-    console.log(url);
     fetch(url)
     .then(data => data.json())
     .then(data => {
